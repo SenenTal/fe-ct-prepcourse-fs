98f59dfde50e88811cd25c8c750381883472411c
SenenTal <senen660@hotmail.com>
2023-06-30 17:29:15 -0700

    Otro Dolor de cabeza menos

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,61 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   //Nota: Este es el método de funcion constructora para crear la clase:
+   class Usuario{
+      constructor(usuario, nombre, email, password){
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+         this.saludar = function() {
+         return 'Hola, mi nombre es '+this.nombre;
+      }
+   }
+}
+   return Usuario;
+   //Este es el otro método ES6(el de expresion de clase)
+   /*function Usuario(opciones)
+   {
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+   }
+   Usuario.prototype.saludar = function(){
+      return "Nombre: "+Usuario.nombre;
+   }
+   return Usuario;*/
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function(){
+      return "Hello World!";
+   }
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function(){
+      var stringInvertida = "";
+      //Nota: como queremos sacar el reverso, el conteo comienza desde la longitud
+      //hasta el numero 0, haciendo un decrecimiento
+      for (var i = this.length - 1; i >= 0; i--)
+      {
+         //Aqui se esta concatenando con la letra del final, uno por uno
+         //hasta la letra inicial como letra final "Hola" -----> "aloH"
+         stringInvertida = stringInvertida + this[i];
+      }
+      return stringInvertida;
+   }  
+   //Nota: La StringInvertida es un valor dentro de un método, y ese método
+   //debe regresar un valor dentro del método agregarStringInvertida()
+   return String;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,20 +6,49 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor(nombre, apellido, edad, domicilio)
+   {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+      //Nota: this.detalle es el objeto que retorna las propiedades de la persona 
+   } 
+   detalle(){
+      return {
+         nombre: this.nombre,
+         apellido: this.apellido,
+         edad: this.edad,
+         domicilio: this.domicilio
+      }
+   }
 }
+/*var a = new Persona("Senen", "Lord", 24, "Calle Anonima #44");
+console.log(a.detalle());*/
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   var Instancia = new Persona(nombre, apellido, edad, domicilio);
+   //Nota: el error estaba que regresaba un método (Instancia.detalle) en la instancia,
+   //y lo que pide es solo el objeto instanciado
+   return Instancia;
 }
-
+console.log(crearInstanciaPersona("Mar", "Lopez", 20, "Atlantico Oeste"));
 function agregarMetodo() {
    // La función agrega un método "datos" a la clase "Persona".
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   //Nota: dice agregar un nuevo método de clase, no extender, me equivoque con
+   //class datos extends Persona
+   //Nota2: no indica agregar propiedades a la función, por lo que nombre y edad son referencias de
+   //la clase Persona
+   Persona.prototype.datos = function(){
+      return (this.nombre+", "+this.edad+" años");
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
